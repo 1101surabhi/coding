1f9f7f124de77bb89b12fb04a4098576ed817d80
Surabhi Agarwal <92710848+1101surabhi@users.noreply.github.com>
2024-10-12 22:49:42 +0530

    Time: 6 ms (7.61%), Space: 11.2 MB (37.81%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    void solve(vector<int>nums, vector<vector<int>>& ans, vector<int>output, int index){
-        if (index >= nums.size()){
-            ans.push_back(output) ;
-            return ;
+    string convertToString(int x, int n){
+        string ans = "" ;
+        while (n--){
+            string rem = std::to_string(x % 2) ;
+            ans = rem + ans ;
+            x /= 2 ;
         }
-
-        //exclude
-        solve(nums, ans, output, index+1) ;
-
-        //include
-        output.push_back(nums[index]) ;
-        solve(nums, ans, output, index+1) ;
+        return ans ;
     }
+
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans ;
-        vector<int> output ;
-        int index = 0 ;
-        solve(nums, ans, output, index) ;
+        int n = nums.size() ;
+        for (int i=0; i<pow(2, n); i++){
+            string numString = convertToString(i, n) ;
+            vector<int> temp ;
+            for (int j=0; j<n; j++){
+                if (numString[j] == '1'){
+                    temp.push_back(nums[j]) ;
+                }
+            }
+            ans.push_back(temp) ;
+        }
         return ans ;
     }
 };
