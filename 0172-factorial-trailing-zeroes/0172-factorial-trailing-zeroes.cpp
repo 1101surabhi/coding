bf2736aebb2b977ae043506e4e0c7fc780234f07
Surabhi Agarwal <92710848+1101surabhi@users.noreply.github.com>
2025-04-20 19:46:29 +0530

    Time: 0 ms (100%), Space: 8.1 MB (85.69%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        if (n == 0) return 0 ;
-        int count = 0 ;
-        for (int i=5; n/i >= 1; i*=5){
-            count += n/i ;
+        int sum = 0 ;
+        for (int i=5; n/i>=1; i*=5){
+            sum += n/i ;
         }
-        return count ;
+        return sum ;
     }
 };
